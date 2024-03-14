d8b80729043d27f64631e67f8002a215953bd94d
Sergei Moroz <moroz.sergei94@gmail.com>
2024-03-14 20:18:19 +0300

    домашка 4 (полностью)

--- HelloWorld/src/Homework_4.java
+++ HelloWorld/src/Homework_4.java
@@ -1,58 +1,58 @@
 import java.util.Scanner;
-//
-//public class Homework_4 {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        int score;
-//
-//        while (true) {
-//            System.out.print("Please enter your score: ");
-//
-//            if (scanner.hasNextInt()) {
-//                score = scanner.nextInt();
-//
-//                if (score >= 0 && score <= 100) {
-//                    break;
-//                } else {
-//                    System.out.println("Введено некорректное значение. Введите число от 0 до 100.");
-//                }
-//            } else {
-//                System.out.println("Введено некорректное значение. Введите число от 0 до 100.");
-//                scanner.next();
-//            }
-//        }
-//        if (score >= 70) {
-//            System.out.println("Конгратулатионс!");
-//        } else {
-//            System.out.println("Сорян.");
-//        }
-//
-//        scanner.close();
-//    }
-//}
+
+public class Homework_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int score;
+
+        while (true) {
+            System.out.print("Please enter your score: ");
+
+            if (scanner.hasNextInt()) {
+                score = scanner.nextInt();
+
+                if (score >= 0 && score <= 100) {
+                    break;
+                } else {
+                    System.out.println("Введено некорректное значение. Введите число от 0 до 100.");
+                }
+            } else {
+                System.out.println("Введено некорректное значение. Введите число от 0 до 100.");
+                scanner.next();
+            }
+        }
+        if (score >= 70) {
+            System.out.println("Конгратулатионс!");
+        } else {
+            System.out.println("Сорян.");
+        }
+
+        scanner.close();
+    }
+}
 
 
-//public class Homework_4 {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        String input = "";
-//
-//        while (true) {
-//            System.out.print("Please enter something: ");
-//            input = scanner.nextLine();
-//
-//
-//            if (input.equals("Exit")) {
-//                break;
-//            }
-//        }
-//
-//        System.out.println("Exiting the program.");
-//
-//        scanner.close();
-//    }
-//}
+public class Homework_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while (true) {
+            System.out.print("Please enter something: ");
+            input = scanner.nextLine();
+
+
+            if (input.equals("Exit")) {
+                break;
+            }
+        }
+
+        System.out.println("Exiting the program.");
+
+        scanner.close();
+    }
+}
 
 public class Homework_4 {
     public static void main(String[] args) {
