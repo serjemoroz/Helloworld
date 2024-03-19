e0750feb96e2735ba070e3193ced894872c232aa
Sergei Moroz <moroz.sergei94@gmail.com>
2024-03-19 22:12:02 +0300

    домашка №5

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,65 +1,8 @@
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        // [0] - username, [1] - password, [2] - type
-//
-//        User adminUser = new User();
-//        User abcInvestGroup = new User();
-//        User vasiliPoukine = new User();
-//
-//        adminUser.username = "admin";
-//        adminUser.password = "admin";
-//        adminUser.userType = "admin";
-//
-//        abcInvestGroup.username = "abcInvest";
-//        abcInvestGroup.password = "qwerty";
-//        abcInvestGroup.userType = "seller";
-//
-//        vasiliPoukine.username = "vpoupkine";
-//        vasiliPoukine.password = "Test1234";
-//        vasiliPoukine.userType = "buyer";
-//
-//        User loggedInUser = login();
-//
-//        User loggedInUser = new User();
-//        boolean isLoggedIn = false;
-//        Scanner scanner = new Scanner(System.in);
-//
-//        while (!isLoggedIn) {
-//            System.out.print("Введите логин: ");
-//            String userLogin = scanner.nextLine();
-//            System.out.print("Введите пароль: ");
-//            String userPassword = scanner.nextLine();
-//
-//            String[] loggedInUser = new String[3];
-//            for (String[] user : users) {
-//                if (user.equals(userLogin) && user.equals(userPassword)) {
-//                    loggedInUser = user;
-//                    isLoggedIn = true;
-//                    System.out.printf("Welcome, %s! You've logged in successfully!", userLogin);
-//                    break;
-//                }
-//            }
-//            if (!isLoggedIn) {
-//                System.out.println("Sorry, username or password is incorrect");
-//            }
-//        }
-//
-//        switch (loggedInUser)
-//
-//
-//    }
-//}
-//
-//
-//
-
-
 import Homework.Pug;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         // Создаем новый объект класса Pug
         Pug myPug = new Pug("Max", 3, "fawn", "small");
 
