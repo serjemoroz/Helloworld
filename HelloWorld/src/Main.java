2c459383942006a301d3384ec9454533ff189a1f
Sergei Moroz <moroz.sergei94@gmail.com>
2024-03-19 21:40:04 +0300

    --

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -55,18 +55,25 @@
 //
 //
 
+//
+//import Homework.Pug;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        // Создаем новый объект класса Pug
+//        Pug myPug = new Pug("Max", 3, "fawn", "small");
+//
+//        // Выводим информацию о мопсе
+//        System.out.println("My pug's name is " + myPug.getName());
+//        System.out.println("My pug is " + myPug.getAge() + " years old");
+//        System.out.println("My pug has " + myPug.getFurColor() + " fur");
+//        System.out.println("My pug is " + myPug.getSize());
+//    }
+//}
 
-import Homework.Pug;
 
-public class Main {
-    public static void main(String[] args) {
-        // Создаем новый объект класса Pug
-        Pug myPug = new Pug("Max", 3, "fawn", "small");
+import enums.Browser
+        public class
 
-        // Выводим информацию о мопсе
-        System.out.println("My pug's name is " + myPug.getName());
-        System.out.println("My pug is " + myPug.getAge() + " years old");
-        System.out.println("My pug has " + myPug.getFurColor() + " fur");
-        System.out.println("My pug is " + myPug.getSize());
-    }
-}
+
+
